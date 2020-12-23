51cd93e547b1e0bfa345b6fef62f4ef20ffd399b
Yoojin Kim <yoojinkim@Yoojinui-MacBookPro.local>
2020-12-23 23:58:07 +0900

    project setting

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
+import './App.css';
+import {
+  Route,
+  Switch,
+  BrowserRouter as Router,
+} from "react-router-dom";
+import React from 'react';
+import UserProvider from './providers/UserProvider';
 import './App.css';
 
-function App() {
+
+
+function App({history}) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <UserProvider 
+    render={
+      <div className="App">
+        <div className="frame">
+        <Router basename={process.env.PUBLIC_URL}>
+          <Switch>
+            {/* <Route exact path='/' component={LandingPage} /> */}
+          </Switch>
+        </Router>
+        </div>
+      </div>
+    } />
   );
 }
 
