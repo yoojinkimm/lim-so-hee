cc0beed52bd1e93bbe53db1625d549f86f65f24b
Yoojin Kim <yoojinkim@Yoojinui-MacBookPro.local>
2021-11-12 11:32:40 +0900

    fix bug

--- src/data/text/image.js
+++ src/data/text/image.js
@@ -1,40 +1,40 @@
-import Grandma1 from '../../data/images/할머니집1.jpg';
-import Grandma2 from '../../data/images/할머니집2.jpg';
-import Elementary from '../../data/images/초등학교 사진.png';
+import Grandma1 from "../../data/images/할머니집1.jpeg";
+import Grandma2 from "../../data/images/할머니집2.jpg";
+import Elementary from "../../data/images/초등학교 사진.png";
 
-import Daejong from '../../data/images/대종기획_최종.svg';
-import Zewoo from '../../data/images/제우기획_최종.svg';
-import KCA from '../../data/images/KCA_최종.svg';
+import Daejong from "../../data/images/대종기획_최종.svg";
+import Zewoo from "../../data/images/제우기획_최종.svg";
+import KCA from "../../data/images/KCA_최종.svg";
 
 export const image_contents = [
-    {
-        index: 0,
-        title : '할머니 집 사진1.jpeg',
-        url: Grandma1
-    },
-    {
-        index: 1,
-        title : '할머니 집 사진2.jpeg',
-        url: Grandma2
-    },
-    {
-        index: 2,
-        title : '초등학교 사진.jpeg',
-        url: Elementary
-    },
-    {
-        index: 3,
-        title : '대종기획_최종.pdf(1/46페이지)',
-        url: Daejong
-    },
-    {
-        index: 4,
-        title : '제우기획_최종.pdf(1/34페이지)',
-        url: Zewoo
-    },
-    {
-        index: 5,
-        title : 'KCA_최종.pdf(1/27페이지)',
-        url: KCA
-    },
-]
+  {
+    index: 0,
+    title: "할머니 집 사진1.jpeg",
+    url: Grandma1,
+  },
+  {
+    index: 1,
+    title: "할머니 집 사진2.jpeg",
+    url: Grandma2,
+  },
+  {
+    index: 2,
+    title: "초등학교 사진.jpeg",
+    url: Elementary,
+  },
+  {
+    index: 3,
+    title: "대종기획_최종.pdf(1/46페이지)",
+    url: Daejong,
+  },
+  {
+    index: 4,
+    title: "제우기획_최종.pdf(1/34페이지)",
+    url: Zewoo,
+  },
+  {
+    index: 5,
+    title: "KCA_최종.pdf(1/27페이지)",
+    url: KCA,
+  },
+];
